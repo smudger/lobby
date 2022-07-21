f0ec7ceed015e35344d552ffca996bf15565f7f8
Louis Smith <15144861+smudger@users.noreply.github.com>
2022-07-21 16:35:06 +0100

    Add Eslint and Prettier

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,16 +1,20 @@
 import "../css/app.css";
 
-import {createApp, h} from "vue";
-import {createInertiaApp, Head, Link} from "@inertiajs/inertia-vue3";
-import {InertiaProgress} from "@inertiajs/progress";
-import {resolvePageComponent} from "laravel-vite-plugin/inertia-helpers";
+import { createApp, h } from "vue";
+import { createInertiaApp, Head, Link } from "@inertiajs/inertia-vue3";
+import { InertiaProgress } from "@inertiajs/progress";
+import { resolvePageComponent } from "laravel-vite-plugin/inertia-helpers";
 
 const appName =
     window.document.getElementsByTagName("title")[0]?.innerText || "Laravel";
 
 createInertiaApp({
     title: (title) => `${title} - ${appName}`,
-    resolve: (name) => resolvePageComponent(`./Pages/${name}.vue`, import.meta.glob("./Pages/**/*.vue")),
+    resolve: (name) =>
+        resolvePageComponent(
+            `./Pages/${name}.vue`,
+            import.meta.glob("./Pages/**/*.vue")
+        ),
     setup({ el, app, props, plugin }) {
         return createApp({ render: () => h(app, props) })
             .use(plugin)
--- routes/web.php
+++ routes/web.php
@@ -2,4 +2,4 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::inertia('/', 'Home');
+Route::inertia('/', 'TheHome');
